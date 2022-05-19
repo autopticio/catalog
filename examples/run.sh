1628a9bc5d5a0336f6a62a6def91b45c66e36d37
autopticio <support@autoptic.com>
2022-05-18 19:05:59 -0500

    helper script for accessing endpoints

---
+++ examples/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Input params
+#1 PQL environment definition json 
+#2 PQL program file
+#3 PQL endpoint URL
+
+curl  -H "content-type: application/json" -X POST  \
+--data '{"vars": "'$(cat $1 | base64 )'", "pql": "'$(cat $2 | base64 )'"}' $3 
