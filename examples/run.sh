49bd7d9c935cfe256027f6d7457bbf7c08957aab
autopticio <support@autoptic.com>
2022-12-30 10:29:56 -0600

    New correlate, chart and env properties,update to where property references

--- examples/run.sh
+++ examples/run.sh
@@ -5,5 +5,5 @@
 #2 PQL program file
 #3 PQL endpoint URL
 
-curl  -H "content-type: application/json" -X POST  \
+curl -H "content-type: application/json" -X POST  \
 --data '{"vars": "'$(cat $1 | base64 )'", "pql": "'$(cat $2 | base64 )'"}' $3 
