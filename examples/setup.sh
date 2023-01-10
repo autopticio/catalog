4ec8c2d98c377983ae116a8d52940a529770484c
autopticio <support@autoptic.com>
2023-01-09 18:53:08 -0700

    new setup script

---
+++ examples/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AUTOPTIC_HOME=~/autoptic
+AUTOPTIC_RUN=$AUTOPTIC_HOME/run.sh
+AUTOPTIC_ENV=$AUTOPTIC_HOME/env.json
+AUTOPTIC_PQL=$AUTOPTIC_HOME/simple.pql
+
+curl -s --create-dirs -o $AUTOPTIC_RUN "https://raw.githubusercontent.com/autopticio/catalog/main/examples/run.sh"
+curl -s --create-dirs -o $AUTOPTIC_ENV "https://raw.githubusercontent.com/autopticio/catalog/main/examples/env_cw.json"
+curl -s --create-dirs -o $AUTOPTIC_PQL "https://raw.githubusercontent.com/autopticio/catalog/main/examples/simple.pql"
+
+echo "Run your first PQL program!"
+echo "sh $AUTOPTIC_RUN $AUTOPTIC_ENV $AUTOPTIC_PQL $AUTOPTIC_EP"
