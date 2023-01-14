5265d22b191d01fd5eba6d23622d48e52e796143
autopticio <support@autoptic.com>
2023-01-13 19:44:12 -0600

    changed order of params in run and setup

--- examples/run.sh
+++ examples/run.sh
@@ -2,8 +2,9 @@
 
 # Input params
 #1 PQL environment definition json 
-#2 PQL program file
-#3 PQL endpoint URL
+#2 PQL endpoint URL
+#3 PQL program file
+AUTOPTIC_EP="AUTOPTIC_EP_VAL"
 
 curl -H "content-type: application/json" -X POST  \
---data '{"vars": "'$(cat $1 | base64 )'", "pql": "'$(cat $2 | base64 )'"}' $3 
+--data '{"vars": "'$(cat $1 | base64 )'", "pql": "'$(cat $3 | base64 )'"}' $2 
