3e65d9531ae1946cd2663da1aa9c1f668ba190d8
autopticio <support@autoptic.com>
2023-01-15 08:34:37 -0600

    Simplified setup and run scripts to use env vars

--- examples/run.sh
+++ examples/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Input params
-#1 PQL environment definition json 
-#2 PQL endpoint URL
-#3 PQL program file
-AUTOPTIC_EP="AUTOPTIC_EP_VAL"
+PQL_PROGRAM=$1
 
+if [ -z "$AUTOPTIC_EP" ] || [ -z "$PQL_PROGRAM" ] || [ -z "$AUTOPTIC_ENV" ]
+then
+	echo "Missing inputs: AUTOPTIC_ENV or AUTOPTIC_EP environment variables were not set, or a PQL program  was not provided as an input parameter."
+	echo "example ..."
+else
 curl -H "content-type: application/json" -X POST  \
---data '{"vars": "'$(cat $1 | base64 )'", "pql": "'$(cat $3 | base64 )'"}' $2 
+--data '{"vars": "'$(cat $AUTOPTIC_ENV | base64 )'", "pql": "'$(cat $PQL_PROGRAM | base64 )'"}' $AUTOPTIC_EP 
+fi
+
