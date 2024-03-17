83a406d7c6fede87eeede416ee1ea5712ee9cf45
autopticio <support@autoptic.com>
2024-03-17 09:13:15 -0500

    improved bash handling for different linux distros

--- examples/setup.sh
+++ examples/setup.sh
@@ -9,6 +9,7 @@ curl -s --create-dirs -o $AUTOPTIC_ENV "https://raw.githubusercontent.com/autopt
 curl -s --create-dirs -o $AUTOPTIC_PQL "https://raw.githubusercontent.com/autopticio/catalog/main/examples/_core/demo.pql"
 echo "AUTOPTIC_EP=$AUTOPTIC_EP" > $AUTOPTIC_RUN
 echo "$(cat $AUTOPTIC_TRUN)" >> $AUTOPTIC_RUN
+chmod u+x $AUTOPTIC_RUN
 rm $AUTOPTIC_TRUN
 
 echo "-------------------------------------------------------------------------------------"
@@ -16,7 +17,7 @@ echo "Let's get started!"
 echo "1. Add your AWS keys with CloudWatch access to $AUTOPTIC_ENV"
 echo "2. Run a simple demo PQL program to query EC2 CPUUtilization from CloudWatch and chart the results:"
 echo ""
-echo "sh $AUTOPTIC_RUN $AUTOPTIC_PQL"
+echo "$AUTOPTIC_RUN $AUTOPTIC_PQL"
 echo ""
 echo "3. Find more examples and docs in our public repo https://github.com/autopticio/catalog"
 echo "Happy PQL-ing!"
